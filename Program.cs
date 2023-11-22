c6a3c96e24ece9ba4adc00a0d7016972c30006ea
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2023-11-22 16:18:12 +0100

    Fixed API calls.

--- Program.cs
+++ Program.cs
@@ -7,15 +7,17 @@ namespace ArduinoTranslator;
 internal static class Program
 {
     // API Server url
-    private static readonly Uri Uri = new ("http://192.168.160.53/controls/0");
+    private static readonly Uri Uri = new ("http://192.168.160.53/api/controls/0");
 
     // Set the host and port to listen on
 //#error Enter IP Adress (comment this line when entered) !!!
 #warning Check IP Adress
-    private const string Host = "192.168."; // Listen on all available interfaces
+    private const string Host = "0.0.0.0"; // Listen on all available interfaces
     private const int    Port = 2000;     // Choose a port number
 
-    private static void Main()
+    private static void Main() => Start().GetAwaiter().GetResult();
+
+    private static async Task Start()
     {
         // Create an IP endpoint
         var endPoint = new IPEndPoint(IPAddress.Parse(Host), Port);
@@ -28,65 +30,66 @@ internal static class Program
         Console.WriteLine($"Listening on {Host}:{Port}...");
 
         // ReSharper disable once AsyncVoidLambda
-        new Task(async () =>
+
+        while (true)
         {
-            while (true)
+            // Wait for a connection
+            var client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine($"Accepted connection from {(client.Client.RemoteEndPoint as IPEndPoint)?.Address}");
+
+            var stream = client.GetStream();
+
+            try
             {
-                // Wait for a connection
-                var client = await listener.AcceptTcpClientAsync();
-                Console.WriteLine(
-                    $"Accepted connection from {(client.Client.RemoteEndPoint as IPEndPoint)?.Address}");
+                // Receive data from the client (assuming string data)
+                var buffer    = new byte[1024];
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+                var data = Encoding.UTF8.GetString(buffer, 0, bytesRead); // Example of input: "put {\"id\":0,\"next_product\":0,\"current_position\":0,\"halt\":false}"
 
-                var stream = client.GetStream();
-
-                try
+                if (!string.IsNullOrEmpty(data))
                 {
-                    // Receive data from the client (assuming string data)
-                    //var buffer    = new byte[1024];
-                    //var bytesRead = stream.Read(buffer, 0, buffer.Length);
-                    var data = "{{id:0, halt:0, next_product:0, current_position:0}}"; //Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    //Console.WriteLine($"Received data: {data}");
 
-                    if (!string.IsNullOrEmpty(data))
+                    if (data.ToLower().StartsWith("get"))
                     {
-                        Console.WriteLine($"Received data: {data}");
+                        // Send a response back to the client (optional)
+                        var response = await MakeGetApiCall();
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        //stream.Write(responseBytes, 0, responseBytes.Length);
+                    }
 
-                        if (data.ToLower().StartsWith("get"))
-                        {
-                            // Send a response back to the client (optional)
-                            var response      = await MakeGetApiCall();
-                            var responseBytes = Encoding.UTF8.GetBytes(response);
-                            stream.Write(responseBytes, 0, responseBytes.Length);
-                        }
-
-                        if (data.ToLower().StartsWith("put"))
-                        {
-                            await MakePutApiCall(data);
-                        }
+                    if (data.ToLower().StartsWith("put"))
+                    {
+                        data = data.Remove(0, 3);
+                        Console.WriteLine(data);
+                        await MakePutApiCall(data);
                     }
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Error processing data: {e.Message}");
-                }
-                finally
-                {
-                    // Close the connection
-                    client.Close();
-                    Console.WriteLine(
-                        $"Connection with {(client.Client.RemoteEndPoint as IPEndPoint)?.Address} closed");
-                }
             }
-        }).Start();
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error processing data: {e.Message}");
+            }
+            finally
+            {
+                // Close the connection
+                client.Close();
+                Console.WriteLine(
+                    $"Connection with {(client.Client.RemoteEndPoint as IPEndPoint)?.Address} closed");
+            }
+        }
     }
 
     private static async Task MakePutApiCall(string data)
     {
-        var content = new StringContent(data);
+        var content = new StringContent(data, Encoding.UTF8, "application/json");
 
         using var httpClient = new HttpClient();
 
         var response = await httpClient.PutAsync(Uri, content);
         response.EnsureSuccessStatusCode();
+
+        Console.WriteLine($"Put call made with: {content}");
     }
 
     private static async Task<string> MakeGetApiCall()
@@ -95,6 +98,8 @@ internal static class Program
 
         var response = await httpClient.GetAsync(Uri);
         response.EnsureSuccessStatusCode();
+
+        Console.WriteLine($"Get call made with: {await response.Content.ReadAsStringAsync()}");
 
         return await response.Content.ReadAsStringAsync();
     }
