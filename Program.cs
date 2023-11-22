bcd66ee0fd052aeeb62ddf18d19651d0a890a0c7
Thomas V <thomasvalkenburg1@gmail.com>
2023-11-22 16:44:47 +0100

    More debug and uncommented used code

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ namespace ArduinoTranslator;
 internal static class Program
 {
     // API Server url
-    private static readonly Uri Uri = new ("http://192.168.160.53/api/controls/0");
+    private static readonly Uri Uri = new("http://192.168.160.53/api/controls/0");
 
     // Set the host and port to listen on
 //#error Enter IP Adress (comment this line when entered) !!!
 #warning Check IP Adress
     private const string Host = "0.0.0.0"; // Listen on all available interfaces
-    private const int    Port = 2000;     // Choose a port number
+    private const int    Port = 2000;      // Choose a port number
+
+    //private static int _count = 0;
 
     private static void Main() => Start().GetAwaiter().GetResult();
 
@@ -44,18 +46,20 @@ internal static class Program
                 // Receive data from the client (assuming string data)
                 var buffer    = new byte[1024];
                 var bytesRead = stream.Read(buffer, 0, buffer.Length);
-                var data = Encoding.UTF8.GetString(buffer, 0, bytesRead); // Example of input: "put {\"id\":0,\"next_product\":0,\"current_position\":0,\"halt\":false}"
+                var data      = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                // Example of input: "put {\"id\":0,\"next_product\":null,\"current_position\":0,\"halt\":false}" or "get"
+                //var data = "put {\"id\":0,\"next_product\":null,\"current_position\":0,\"halt\":false}";
 
                 if (!string.IsNullOrEmpty(data))
                 {
-                    //Console.WriteLine($"Received data: {data}");
+                    Console.WriteLine($"Received data: {data}");
 
                     if (data.ToLower().StartsWith("get"))
                     {
                         // Send a response back to the client (optional)
-                        var response = await MakeGetApiCall();
+                        var response      = await MakeGetApiCall();
                         var responseBytes = Encoding.UTF8.GetBytes(response);
-                        //stream.Write(responseBytes, 0, responseBytes.Length);
+                        stream.Write(responseBytes, 0, responseBytes.Length);
                     }
 
                     if (data.ToLower().StartsWith("put"))
@@ -74,8 +78,7 @@ internal static class Program
             {
                 // Close the connection
                 client.Close();
-                Console.WriteLine(
-                    $"Connection with {(client.Client.RemoteEndPoint as IPEndPoint)?.Address} closed");
+                Console.WriteLine($"Connection with {(client.Client.RemoteEndPoint as IPEndPoint)?.Address} closed");
             }
         }
     }
